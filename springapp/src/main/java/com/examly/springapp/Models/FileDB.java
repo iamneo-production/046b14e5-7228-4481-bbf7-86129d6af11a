5d8a14ee0851fd3b6a287240656c8ebffec49340
kaviyashree-22 <kaviya.kavzz22@gmail.com>
2022-03-26 15:25:41 +0000

    hj

--- springapp/src/main/java/com/examly/springapp/Models/FileDB.java
+++ springapp/src/main/java/com/examly/springapp/Models/FileDB.java
@@ -1,74 +1,74 @@
-package com.examly.springapp.Models;
+// package com.examly.springapp.Models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Table;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.Id;
+// import javax.persistence.Lob;
+// import javax.persistence.Table;
 
-import org.hibernate.annotations.GenericGenerator;
+// import org.hibernate.annotations.GenericGenerator;
 
-@Entity
-@Table(name = "files")
-public class FileDB 
-{
+// @Entity
+// @Table(name = "files")
+// public class FileDB 
+// {
 
-	@Id
-	@GeneratedValue(generator = "uuid")
-	@GenericGenerator(name = "uuid", strategy = "uuid2")
-	private String id;
+// 	@Id
+// 	@GeneratedValue(generator = "uuid")
+// 	@GenericGenerator(name = "uuid", strategy = "uuid2")
+// 	private String id;
 
-	private String name;
+// 	private String name;
 
-	private String type;
+// 	private String type;
 
-	@Lob
-	private byte[] data;
+// 	@Lob
+// 	private byte[] data;
 
-	public FileDB() 
-  {
-	}
+// 	public FileDB() 
+//   {
+// 	}
 
-	public FileDB(String name, String type, byte[] data) 
-  {
-		this.name = name;
-		this.type = type;
-		this.data = data;
-	}
+// 	public FileDB(String name, String type, byte[] data) 
+//   {
+// 		this.name = name;
+// 		this.type = type;
+// 		this.data = data;
+// 	}
 
-	public String getId() 
-  {
-		return id;
-	}
+// 	public String getId() 
+//   {
+// 		return id;
+// 	}
 
-	public String getName() 
-  {
-		return name;
-	}
+// 	public String getName() 
+//   {
+// 		return name;
+// 	}
 
-	public void setName(String name) 
-  {
-		this.name = name;
-	}
+// 	public void setName(String name) 
+//   {
+// 		this.name = name;
+// 	}
 
-	public String getType() 
-  {
-		return type;
-	}
+// 	public String getType() 
+//   {
+// 		return type;
+// 	}
 
-	public void setType(String type) 
-  {
-		this.type = type;
-	}
+// 	public void setType(String type) 
+//   {
+// 		this.type = type;
+// 	}
 
-	public byte[] getData() 
-  {
-		return data;
-	}
+// 	public byte[] getData() 
+//   {
+// 		return data;
+// 	}
 
-	public void setData(byte[] data) 
-  {
-		this.data = data;
-	}
+// 	public void setData(byte[] data) 
+//   {
+// 		this.data = data;
+// 	}
 
-}
+// }
